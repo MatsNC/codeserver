648f5af9f869da826e28b9c9d345226d9bcd407a
MatsNC <64698729+MatsNC@users.noreply.github.com>
2024-03-24 20:27:45 -0300

    Sprint3 - Tercer desafío: Server con Express (#3)
    * add_resources
    
    * add_resources
    
    * add_resources
    
    * add_resources
    
    * add_resources
    
    * sprint2
    
    * Se trabaja en Desafio 2
    
    * Se completa hasta metodo read
    
    * sprint2
    
    * sprint2
    
    * sprint2
    
    * sprint2
    
    * sprint3
    
    * sprint3
    
    * se agregan y se prueban funcionalidades
    
    * se agregan y se prueban funcionalidadesclear
    
    * Update Readme.md
    
    * sprint3
    
    * sprint3
    
    ---------
    
    Co-authored-by: Mats14_83 <Mats14_83@bitbucket.org>

--- data/memory/UserManager.memory.js
+++ data/memory/UserManager.memory.js
@@ -1,34 +1,32 @@
-const crypto = require("crypto");
-class UserManager {
+import crypto from "crypto";
+export default class UserManager {
   static #users = [];
-  //Metodo para crear un Usuario:
+  //Method to create a new User:
   create(data) {
     try {
       const user = {
         id: data.id || crypto.randomBytes(12).toString("hex"),
-        photo: data.photo || "foto_default.jpg",
+        photo: data.photo || "photo_default.jpg",
         email: data.email,
         password: data.password,
         role: data.role,
       };
 
       if (!data.email || !data.password) {
-        throw new Error(
-          "No se pudo crear el usuario. Ingrese email y password"
-        );
+        throw new Error("User not created. Please enter email and password");
       } else {
         UserManager.#users.push(user);
-        console.log("Usuario Creado");
+        console.log("User created successfully");
       }
     } catch (error) {
       console.log(error);
     }
   }
-  //Metodo para leer listado de usuarios:
+  //Method to read Users List:
   read() {
     try {
       if (UserManager.#users.length === 0) {
-        throw new Error("No hay productos cargados");
+        throw new Error("No Users");
       } else {
         return UserManager.#users;
       }
@@ -36,25 +34,25 @@ class UserManager {
       console.log(error);
     }
   }
-  //Metodo para leer un usuario en especifico:
-   readOne(id) {
+  //Method to find a user by id:
+  readOne(id) {
     try {
       const findUser = UserManager.#users.find((user) => user.id === id);
       if (findUser !== undefined) {
         return findUser;
       } else {
-        throw new Error("Usuario no encontrado");
+        throw new Error("User not found");
       }
     } catch (error) {
       console.log(error);
     }
   }
-  //Metodo para borrar un usuario:
+  //Method to destroy a user:
   destroy(id) {
     try {
       let destroyUser = this.readOne(id);
       if (!destroyUser) {
-        throw new Error("Usuario no encontrado");
+        throw new Error("User not found");
       } else {
         const newArray = UserManager.#users.filter((user) => user.id !== id);
         UserManager.#users = newArray;
@@ -63,45 +61,45 @@ class UserManager {
     } catch (error) {
       console.log(error);
     }
-  } 
+  }
 }
 
-const gestorDeUsuario = new UserManager();
-gestorDeUsuario.create({
+const oneUser = new UserManager();
+oneUser.create({
   photo: "foto_usr1.jpg",
   email: "rodri_perez@gmail.com",
   password: "RoDP#5870*!",
   role: "Admin",
 });
 
-gestorDeUsuario.create({
+oneUser.create({
   photo: "foto_usr2.jpg",
   email: "matspind_45@gmail.com",
   password: "654!La3p2L1*",
   role: "Key User",
 });
 
-gestorDeUsuario.create({
+oneUser.create({
   photo: "foto_usr3.jpg",
   email: "maria@gmail.com",
   password: "L#2vF6Qz@",
-  role: "Client"
+  role: "Client",
 });
 
-gestorDeUsuario.create({
+oneUser.create({
   id: "123456789",
   photo: "foto_usr4.jpg",
   email: "juan_carlos@yahoo.com",
   password: "Gp5@jR9d!",
-  role: "Admin"
+  role: "Admin",
 });
 
-console.log("Listado de Usuarios:");
-console.log(gestorDeUsuario.read());
-console.log("Usuario buscado:");
-console.log(gestorDeUsuario.readOne("123456789"));
-console.log("Usuario borrado:");
-console.log(gestorDeUsuario.destroy("123456789"));
-console.log("Listado actualizado:");
-console.log(gestorDeUsuario.read());
+console.log("List of Users:");
+console.log(oneUser.read());
+console.log("User found:");
+console.log(oneUser.readOne("123456789"));
+console.log("User deleted:");
+console.log(oneUser.destroy("123456789"));
+console.log("Updated List:");
+console.log(oneUser.read());
 
---
+++ server.js
@@ -0,0 +1,136 @@
+import express from "express";
+import Productos from "./data/fs/files/ProductManager.fs.js";
+import Users from "./data/fs/files/UserManager.fs.js";
+
+const server = express();
+
+const PORT = 3500;
+
+const ready = () => console.log("server ready on port " + PORT);
+server.listen(PORT, ready);
+
+//middleware:
+server.use(express.urlencoded({ extended: true }));
+
+//Server for products
+
+const prod_route = "/api/products";
+const index_route = "/";
+const params_route = "/api/products/:pid";
+
+async function read_products(req, res) {
+  try {
+    const { category } = req.query;
+    const all_products = await Productos.read(category);
+    if (all_products.length > 0) {
+      res.status(200).json({
+        success: true,
+        response: all_products,
+      });
+    } else {
+      const error = new Error("Not Found");
+      error.statusCode = 404;
+      throw error;
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(error.statusCode).json({
+      success: false,
+      message: error.message,
+    });
+  }
+}
+
+async function index_function(req, res) {
+  try {
+    res.status(200).json({ success: true, message: "PC Store Web Server" });
+  } catch (error) {
+    console.log(error);
+    res
+      .status(500)
+      .json({ success: false, message: "Failed to load web page" });
+  }
+}
+
+async function params_function(req, res) {
+  try {
+    const { pid } = req.params;
+    const one = await Productos.readOne(pid);
+    if (one) {
+      return res.status(200).json({
+        response: one,
+        success: true,
+      });
+    } else {
+      const error = new Error("NOT FOUND");
+      error.statusCode = 404;
+      throw error;
+    }
+  } catch (error) {
+    console.log(error);
+    return res.status(error.statusCode).json({
+      response: error.message,
+      success: false,
+    });
+  }
+}
+
+server.get(prod_route, read_products);
+server.get(index_route, index_function);
+server.get(params_route, params_function);
+
+//////////////////////////////////////////////////////////////
+
+//Server for users
+const users_route = "/api/users";
+const params_users_route = "/api/users/:uid";
+
+async function read_users(req, res) {
+  try {
+    const { role } = req.query;
+    const all_users = await Users.read(role);
+    if (all_users.length > 0) {
+      res.status(200).json({
+        success: true,
+        response: all_users,
+      });
+    } else {
+      const error = new Error("Not Found");
+      error.statusCode = 404;
+      throw error;
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(error.statusCode).json({
+      success: false,
+      message: error.message,
+    });
+  }
+}
+
+async function params_users(req, res) {
+  try {
+    const { uid } = req.params;
+    const one = await Users.readOne(uid);
+    if (one) {
+      return res.status(200).json({
+        response: one,
+        success: true,
+      });
+    } else {
+      const error = new Error("NOT FOUND");
+      error.statusCode = 404;
+      throw error;
+    }
+  } catch (error) {
+    console.log(error);
+    return res.status(error.statusCode).json({
+      response: error.message,
+      success: false,
+    });
+  }
+}
+
+server.get(users_route, read_users);
+
+server.get(params_users_route, params_users);
