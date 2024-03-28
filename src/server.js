c985131be0e8cd6d0279e34f759669f02739145b
MatsNC <matscorrea333@gmail.com>
2024-03-27 22:32:35 -0300

    09_hdbs

--- src/server.js
+++ src/server.js
@@ -2,16 +2,25 @@ import express from 'express';
 import indexRouter from './router/index.router.js';
 import errorHandler from './middlewares/errorHandler.mid.js';
 import pathHandler from './middlewares/pathHandler.mid.js';
+import morgan from 'morgan';
+import {engine} from 'express-handlebars';
+import __dirname from './utils.js';
 
 const server = express();
 const PORT = 8080;
 
 const ready = () => console.log('server ready on port ' + PORT);
-
 server.listen(PORT, ready);
+
+//template engine
+server.engine('handlebars',engine());
+server.set('view engine', 'handlebars');
+server.set('views', __dirname + '/src/views');
 
 server.use(express.json());
 server.use(express.urlencoded({ extended: true }));
+server.use(morgan('dev'));
+
 //endpoints
 server.use('/', indexRouter);
 server.use(errorHandler);
